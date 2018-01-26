b03b874b069e235022778e99f34dce1b487bc26a
Asad Hasen <d-asha1@Dev.Green.Net>
2018-01-26 17:07:55 +0000

    adding search result test

---
+++ AngularTesting/pagechecks/searchREsultsPanelChecks.ts
@@ -0,0 +1,6 @@
+﻿import { expect } from "chai";
+import * as searchResultsPanel from "../pageobjects/searchResultsPanel";
+
+export async function checkOtherSearchResults(result: string) {
+    expect(searchResultsPanel.getOtherSearchResults()).to.eventually.
+}
